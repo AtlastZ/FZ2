1e3a5653b6bed533916c38f991c5e726d98e72a3
AnalyzyA <analyzya@gmail.com>
2021-11-24 18:35:57 +0700

    main

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Friend❤️Zone</title>
-    <link rel="icon" href="assets/images/icon.png" type="image/x-icon">
+    <link rel="icon" href="assets/images/Icon.png" type="image/x-icon">
     <link rel="stylesheet" href="css/indexstyle.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -17,7 +17,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
 </head>
 
-<body>
+<body >
     
     <div class="container-fluid index">
         <div class="position-absolute top-50 start-50 translate-middle">
