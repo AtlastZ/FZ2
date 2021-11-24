bdb3f65f9c34f854a47c89b4a05b0c53235d32e4
AnalyzyA <analyzya@gmail.com>
2021-11-24 20:49:42 +0700

    main

--- content/Main.php
+++ content/Main.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 , maximum-scale=1.0, user-scalable=no">
     <title>F❤️N Homepage</title>
     <link rel="icon" href="../assets/images/Icon.png" type="image/x-icon">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
     <link href="../css/page2.css" rel="stylesheet">
+    <link href="../css/nav.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
@@ -18,7 +19,7 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-custom navbar-dark sticky-top"
+    <nav class="navbar navbar-expand-lg navbar-custom navbar-dark sticky-top"
         style="background-color: #73558b; width:100%;">
         <div class="container-fluid ">
             <a class="navbar-brand" href="../index.php" style="width:15%" id="navIcon"><img
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 , maximum-scale=1.0, user-scalable=no">
     <title>Friend❤️Zone</title>
     <link rel="icon" href="assets/images/Icon.png" type="image/x-icon">
     <link rel="stylesheet" href="css/indexstyle.css">
@@ -17,8 +17,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
 </head>
 
-<body >
-    
+<body style="overflow:hidden;">
     <div class="container-fluid index">
         <div class="position-absolute top-50 start-50 translate-middle">
             <section class="fs-2 logo">
