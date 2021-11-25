b0955ed2984333b64396463d6e86c6cff5544f08
AnalyzyA <analyzya@gmail.com>
2021-11-25 18:12:51 +0700

    main'

--- content/ChooseFriend.php
+++ content/ChooseFriend.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0 , maximum-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 , maximum-scale=1.0">
     <title>F❤️Z Friend Gallery</title>
     <link rel="icon" href="../assets/images/Icon.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
--- content/complete.php
+++ content/complete.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0 , maximum-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 , maximum-scale=1.0">
     <title>Thank you F❤️Z</title>
     <link rel="icon" href="../assets/images/Icon.png" type="image/x-icon">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
